02c6534353f32164d9dcaea4010e4c6d1c82f38c
Leo-Quiroga <hleonardoquirogab@hotmail.com>
2023-08-24 16:10:46 -0500

    Proyecto Finalizado

--- Assets/Scripts/ScriptsProfe/MetaDoor/MetaPuerta.cs
+++ Assets/Scripts/ScriptsProfe/MetaDoor/MetaPuerta.cs
@@ -4,24 +4,14 @@ using UnityEngine;
 
 public class MetaPuerta : MonoBehaviour
 {
-    //public AudioClip puertaAuido;
-    //public AudioSource audioSource;
-
 
     public int numCollectable1;
-    //= LevelManager.instance.elementsColected.ToString();
+
     public int numCollectable2;
 
-    //public bool evaluarCollectable1;
-    //public bool evaluarCollectable2;
-    void Start()
-    {
-        
-        //numCollectable1 = 10;
-        //numCollectable2 = 10;
-        Debug.Log(numCollectable1);
-        Debug.Log(numCollectable2);
-    }
+    [SerializeField] private Animator padrePuerta;
+
+    
    
 
 
@@ -34,55 +24,18 @@ public class MetaPuerta : MonoBehaviour
         {
             if (numCollectable1 >= 10 && numCollectable2 >= 10)
             {
-                //Encuentro el objeto Puntaje en el juego
-                //GameObject puntaje = GameObject.FindObjectOfType<Puntaje>().gameObject;
-                //si el puntaje es cierto valor
-                //if (numCollectable1 >= 10)
+               
+                     padrePuerta.Play("door");
 
-                // {
-                Debug.Log(numCollectable1);
-                Debug.Log(numCollectable2);
+                //gameObject.GetComponent<Animator>().Play("door");
+            }
+            else{
                 
-                //audioSource.PlayOneShot(puertaAuido);
-                    //ejecutar aniamcion "door"
-                    gameObject.GetComponent<Animator>().Play("door");
-               // }
-               // else
-               // {
-                    //Debug.Log("Te elementos por recoger.");
-               // }
-            //}
-           // else if (evaluarCollectable2)
-            //{
-                //Encuentro el objeto Puntaje en el juego
-                //GameObject llaves = GameObject.FindObjectOfType<KeysCanvas>().gameObject;
-                //si el puntaje es cierto valor
-               // if (numCollectable2 >=10)
-               // {
-               //     audioSource.PlayOneShot(puertaAuido);
-                    //ejecutar aniamcion "door"
-                //    gameObject.GetComponent<Animator>().Play("door");
-                }
-                else
-                {
-                Debug.Log(numCollectable1);
-                Debug.Log(numCollectable2);
                 Debug.Log("Te faltan elementos por recoger.");
-                }
-           // }
-           // else
-           // {
-              //  audioSource.PlayOneShot(puertaAuido);
-                //ejecutar aniamcion "door"
-              //  gameObject.GetComponent<Animator>().Play("door");
-
-           // }
+            }
+         
 
         }
     }
-    //public void Destruir()
-    //{
-      //  Destroy(gameObject);
-
-    //}
+  
 }
